c8cf8a1a1022b1927dd9efb6b547dfc5d9442b75
xuzequn <xzqyymt@126.com>
2022-08-03 18:10:05 +0800

    0803

--- cartorder_srv/biz/order.go
+++ cartorder_srv/biz/order.go
@@ -3,7 +3,6 @@ package biz
 import (
 	"context"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"gorm.io/gorm/utils"
 	"mic-trainning-lesson-part4/cartorder_srv/model"
 	"mic-trainning-lesson-part4/internal"
 	"mic-trainning-lesson-part4/proto/pb"
@@ -23,7 +22,14 @@ func (s CartOrderServer) OrderList(ctx context.Context, item *pb.OrderPagingReq)
 	}).Count(&total)
 	res.Total = int32(total)
 
-	internal.DB.Scopes(utils.Paginate)
+	internal.DB.Where(&model.OrderItem{
+		AccountId: item.AccountId,
+	}).Scopes(internal.MyPaging(int(item.PageNo), int(item.PageSize))).Find(&orderList)
+	for _, item := range orderList {
+		r := ConventOrderModel2Pb(item)
+		res.ItemList = append(res.ItemList, r)
+	}
+	return &res, nil
 
 }
 
@@ -35,4 +41,20 @@ func (s CartOrderServer) OrderDetail(ctx context.Context, item *pb.OrderItemReq)
 func (s CartOrderServer) ChangeOrderStatus(ctx context.Context, status *pb.OrderStatus) (*emptypb.Empty, error) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func ConventOrderModel2Pb(o model.OrderItem) *pb.OrderItemRes {
+	res := pb.OrderItemRes{
+		Id:        o.ID,
+		AccountId: o.AccountId,
+		PayType:   o.PayType,
+		OrderNum:  o.OrderNo,
+		PostCode:  o.PostCode,
+		Amount:    o.OrderAmount,
+		Addr:      o.Addr,
+		Receiver:  o.Receiver,
+		Mobile:    o.ReceiverMobile,
+		Status:    o.Status,
+	}
+	return &res
 }
