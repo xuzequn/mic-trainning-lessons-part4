9984951572dc1f6494f14cef9a865a17c89dcdcc
xuzequn <xzqyymt@126.com>
2022-08-09 16:35:34 +0800

    0809

--- cartorder_web/order/order.go
+++ cartorder_web/order/order.go
@@ -5,35 +5,40 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"mic-trainning-lesson-part4/cartorder_web/req"
 	"mic-trainning-lesson-part4/internal"
 	"mic-trainning-lesson-part4/proto/pb"
 	"net/http"
+	"strconv"
 )
 
 func ListHandler(c *gin.Context) {
-	//accountId, _ := c.Get("accountId")
-	//claims, _ := c.Get("claims")
-	//pageNoStr := c.DefaultQuery("pageNo", "0")
-	//pageNo, _ := strconv.Atoi(pageNoStr)
-	//
-	//pageSizeStr := c.DefaultQuery("pageSize", "0")
-	//pageSize, _ := strconv.Atoi(pageSizeStr)
-	//
-	//reqPb := &pb.OrderPagingReq{
-	//	PageNo:   int32(pageNo),
-	//	PageSize: int32(pageSize),
-	//}
-	//
-	//// 如果是管理员用户则返回所有订单
-	//customClaims := claims.(*jwt_op.CustromClaims)
-	//if customClaims.AuthorityId == 1 {
-	//	reqPb.AccountId = int32(accountId.(uint))
-	//}
+
+	accountIdStr := c.DefaultQuery("accountId", "0")
+	accountId, _ := strconv.Atoi(accountIdStr)
+
+	if accountId < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "参数错误",
+		})
+	}
+
+	pageNoStr := c.DefaultQuery("pageNo", "0")
+	pageNo, _ := strconv.Atoi(pageNoStr)
+
+	pageSizeStr := c.DefaultQuery("pageSize", "0")
+	pageSize, _ := strconv.Atoi(pageSizeStr)
+
+	reqPb := &pb.OrderPagingReq{
+		PageNo:    int32(pageNo),
+		PageSize:  int32(pageSize),
+		AccountId: int32(accountId),
+	}
+
+	// 如果是管理员用户则返回所有订单
+
 	ctx := context.WithValue(context.Background(), "webContext", c)
-	res, err := internal.OrderClient.OrderList(ctx, &pb.OrderPagingReq{
-		PageNo:   2,
-		PageSize: 1,
-	})
+	res, err := internal.OrderClient.OrderList(ctx, reqPb)
 	if err != nil {
 		zap.S().Error(err)
 		fmt.Println(err)
@@ -61,14 +66,16 @@ func Detail(c *gin.Context) {
 }
 
 func CreateOrder(c *gin.Context) {
+	orderReq := req.OrderReq{}
+	if err := c.ShouldBindJSON(&orderReq); err != nil {
+		zap.S().Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": "内部错误" + err.Error(),
+		})
+	}
 	ctx := context.WithValue(context.Background(), "webContext", c)
-	res, err := internal.OrderClient.CreateOrder(ctx, &pb.OrderItemReq{
-		AccountId: 1,
-		Addr:      "北京",
-		PostCode:  "10010",
-		Receiver:  "xuzequn",
-		Mobile:    "13500000000",
-	})
+	orderItemPb := ConventOrderModel2Pb(orderReq)
+	res, err := internal.OrderClient.CreateOrder(ctx, orderItemPb)
 	if err != nil {
 		zap.S().Error(err)
 		fmt.Println(err)
@@ -77,4 +84,16 @@ func CreateOrder(c *gin.Context) {
 		"msg":     "",
 		"orderNo": res.OrderNum,
 	})
+}
+
+func ConventOrderModel2Pb(req req.OrderReq) *pb.OrderItemReq {
+	return &pb.OrderItemReq{
+		Id:        req.Id,
+		AccountId: req.AccountId,
+		Addr:      req.Addr,
+		PostCode:  req.PostCode,
+		Receiver:  req.Receiver,
+		Mobile:    req.ReceiverMobile,
+		PayType:   req.PayType,
+	}
 }
--- cartorder_web/req/cart_param.go
+++ cartorder_web/req/cart_param.go
@@ -1,9 +1,26 @@
 package req
 
+import "time"
+
 type ShopCartReq struct {
 	Id        int32 `json:"id"`
 	AccountId int32 `json:"accountId" binding:"required"`
 	ProductId int32 `json:"productId" binding:"required"`
 	Num       int32 `json:"num" binding:"required"`
 	Checked   *bool `json:"checked" binding:"required"`
+}
+
+type OrderReq struct {
+	Id             int32  `json:"id"`
+	AccountId      int32  `json:"accountId" binding:"required"`
+	OrderNo        string `json:"orderNo" binding:"required"`
+	PayType        string `json:"payType"` // int wx 银联 支付宝
+	Status         string `json:"status"`  // 未支付 支付成功 超市关闭
+	TradeNo        string `json:"tradeNo"` // 保证双方对账， 第三方对账平台的凭证
+	Addr           string `json:"addr"`
+	Receiver       string `json:"receiver"`
+	ReceiverMobile string `json:"receiverMobile"`
+	PostCode       string `json:"postCode"`
+	OrderAmount    float32
+	PayTime        *time.Time `gorm:"type:datetime"`
 }
